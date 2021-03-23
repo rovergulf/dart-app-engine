c116a3f81d2be1499b44d3f88ff99c212d5e0249
Dmitrii Limonov <disordervlz@gmail.com>
2021-03-24 01:31:11 +0300

    prepare home page for dynamic layout

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dapp_example/pages/home.dart';
+import 'package:dapp_example/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/dapp_example_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -19,6 +20,9 @@ class _ExampleAppState extends State<ExampleApp> {
       home: HomeRoute(),
       debugShowCheckedModeBanner: false,
       onGenerateTitle: (BuildContext context) => AppLocalizations.of(context).appTitle,
+      themeMode: ThemeMode.light,
+      theme: lightTheme,
+      darkTheme: darkTheme,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: const [
         Locale('en'),
--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -4,9 +4,15 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <battery_plus_windows/battery_plus_windows_plugin.h>
+#include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  BatteryPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("BatteryPlusWindowsPlugin"));
+  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
   UrlLauncherPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
 }
